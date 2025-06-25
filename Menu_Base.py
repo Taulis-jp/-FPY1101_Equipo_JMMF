7c58f352c61ece99f8ac36430a04911cf4bff8cd
Magdiel Esther Reynoso Ramírez <ma.reynoso@duocuc.cl>
2025-06-24 22:44:05 -0400

    Agregar funcion datos_magdiel
    Solicito integrar mi funcion datos_magdiel al archivo base.

--- Menu_Base.py
+++ Menu_Base.py
@@ -1,8 +1,11 @@
+def datos_magdiel():
+    print (f"Mi nombre es Magdiel Esther Reynoso Ramirez y tengo 22 años.")
 while True:
     print("\n--- MENÚ PRINCIPAL ---")
     print("1. Función de integrante 1")
     print("2. Función de integrante 2")
     print("3. Función de integrante 3")
+    print("3. Función de integrante 4")
     print("0. Salir")
 
     op = input("Seleccione opción: ")
@@ -13,7 +16,7 @@ while True:
     pass     # Aquí se llamará a la función del integrante 1
     
     elif op == "2":
-    pass # Aquí se llamará a la función del integrante 2
+        datos_magdiel() # Aquí se llamará a la función del integrante 2
     
     elif op == "3":
     pass # Aquí se llamará a la función del integrante 3
